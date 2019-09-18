27cffb8f70ecd46ba7c2243f76256380208541f0
Anton.Sadouski <Anton.Sadouski@DEV-0043>
2019-09-18 10:03:07 +0300

    binding

--- angular-basics/src/app/post3/post3.component.ts
+++ angular-basics/src/app/post3/post3.component.ts
@@ -11,8 +11,14 @@ export class Post3Component implements OnInit {
   arr = [1, 2, 3]
   obj = { a: 1, b: 2}
 
+  img = 'https://ezerus.com.au/wp-content/uploads/2019/05/1_To2H39eauxaeYxYMtV1afQ.png';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      console.log("Timeout is over");
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/250px-Angular_full_color_logo.svg.png'
+    }, 5000)
+  }
 
   ngOnInit() {
   }
