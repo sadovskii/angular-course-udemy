fc2a8e5ab3c734396d7c8c7874c5f6d6a7b35994
Anton.Sadouski <Anton.Sadouski@DEV-0043>
2019-09-19 17:10:09 +0300

    ngStyle and ngClass

--- angular-basics/src/app/post/post.component.ts
+++ angular-basics/src/app/post/post.component.ts
@@ -5,4 +5,6 @@ import {Component} from '@angular/core'
     templateUrl: './post.component.html',
     styleUrls: ['./post.component.scss']
 })
-export class PostComponent{}
+export class PostComponent{
+    backgroundToggle = false;
+}
