874cb2e62ef60b2024deba8d45bc2b205c45a59a
Anton.Sadouski <Anton.Sadouski@DEV-0043>
2019-09-24 15:35:51 +0300

    remove excess

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -3,27 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { GetComponent } from './get/get.component';
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
-import { Get2Component } from './get2/get2.component';
-import { Get3Component } from './base/get3/get3.component';
-import { Post4Component } from './post4/post4.component';
-import { WayBindingComponent } from './way-binding/way-binding.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostComponent,
-    GetComponent,
-    Post2Component,
-    Post3Component,
-    Get2Component,
-    Get3Component,
-    Post4Component,
-    WayBindingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- angular-basics/src/app/get/get.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-get',
-    templateUrl: './get.component.html'
-})
-export class GetComponent {}
